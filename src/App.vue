a72baf8aead666a47d21b3680f369c5965d6235e
Abraham <nencho_1@hotmail.com>
2021-02-14 12:42:58 +0100

    done more style css and finished header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
+  <router-link to="/"></router-link>
+  <router-link to="/main"></router-link>
   <router-view/>
 </template>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto+Slab&display=swap");
 
 :root {
+  --main-font-family: 'Roboto Slab', serif;
   --main-color: #14805e;
+  --secondary-color: #ffdf1b;
+  --text-color: #fff;
 }
 
 * {
@@ -16,6 +22,7 @@
     border: none;
     text-decoration: none;
     list-style: none;
+    font-family: var(--main-font-family);
 }
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,182 +4,525 @@
       <ul>
         <li>
           <figure>
-            <a href="/"><router-link to="/"></router-link><img class="img-logo" alt="Bet365 logo" src="../assets/bet365-logo.png"></a>
+            <router-link to="/"
+              ><img
+                class="img-logo"
+                alt="Bet365 logo"
+                src="../assets/logoMain.png"
+            /></router-link>
           </figure>
         </li>
         <li>
-          <span>Idioma Español</span>
+          <a href="">Deportes</a>
+          <a href="">Directo</a>
         </li>
         <li>
-          <a href="/main"><router-link to="/main"></router-link>Acceder a la pagina</a>
+          <a href="/main">Buscar</a>
+          <a href="">Registrarse</a>
+          <a href="">Iniciar sesión</a>
         </li>
       </ul>
     </nav>
   </header>
 
-  <main>
-    <section>
-      <article>
-        <h4>Bonus de Ingreso</h4>
-        <p>Créditos de apuesta, hasta 100€</p>
-        <h6>Para clientes nuevos</h6>
-        <a href="">Ver últimas cuotas</a>
-        <p>Ingreso mí. 5€. Créd. de apuesta  dispon. una vez determinadas las apuestas
-        del valor de su ingreso válido. Se aplican cuotas mín., exclusiones de apuesta y métodos de pago.
-        Gana. excluyen imp. de créd. de apuesta. Ver lím. de tiempo y condiciones.</p>
-      </article>
+  <div class="grid-container">
+    <main>
+      <section>
+        <article>
+          <h4>Oferta de nuevo cliente</h4>
+          <p class="atom_p_firstChild_bet">Créditos de apuesta, hasta 100€</p>
+          <a href="">Registrate</a>
+          <p class="atom_p_secondChild_bet">
+            Ingreso mí. 5€. Créd. de apuesta dispon. una vez determinadas las
+            apuestas del valor de su ingreso válido. Se aplican cuotas mín.,
+            exclusiones de apuesta y métodos de pago. Gana. excluyen imp. de
+            créd de apuesta. Ver lím. de tiempo y condiciones.
+          </p>
+          <figure>
+            <!-- <img
+              src=""
+              alt="Imagen de jugador de futbol, tenista y jugador de balonmano"
+            /> -->
+          </figure>
+        </article>
+      </section>
 
-      <article>
-        <figure>
-          <img src="../assets/playersMain.png" alt="">
-        </figure>
-      </article>
+      <section>
+        <h4>Directo</h4>
 
-      <article>
-        <h2>Cómo funciona la oferta</h2>
-        <div>
-          <h5>Ingresar</h5>
-        </div>
+        <article>
+        <h3>Futbol 1 x 2</h3>
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
 
-        <div>
-          <h5>Cómo liberar sus créditos de apuesta</h5>
-        </div>
+          <hr>
 
-        <div>
-          <h5>Cómo utilizar los créditos de apuesta</h5>
-        </div>
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
 
-        <a href="">Ver ultimas cuotas</a>
-      </article>
-    </section>
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
 
-    <hr>
-  </main>
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+        </article>
 
-  <footer>
-    <h3>bet365</h3>
+        <article>
+          <h3>Tennis</h3>
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
 
-    <nav>
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+        </article>
+
+        <article>
+          <h3>Baloncesto</h3>
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+        </article>
+      </section>
+
+      <section>
+        <article>
+          <h3>1ª División</h3>
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+        </article>
+
+        <article>
+          <h3>2ª División</h3>
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+        </article>
+
+        <article>
+          <h3>Encuentros destacados</h3>
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+
+          <div>
+            <h5>Lorem ipsum</h5>
+            <h5>Lorem ipsum</h5>
+          </div>
+        </article>
+      </section>
+    </main>
+
+    <aside>
       <ul>
-        <li>
-          <a href="">Ayuda</a>
-        </li>
-
-        <li>
-          <a href="">Banca</a>
-        </li>
-
-        <li>
-          <a href="">Contáctenos</a>
-        </li>
-
-        <li>
-          <a href="">Juego responsable</a>
-        </li>
-
-        <li>
-          <a href="">Afiliados</a>
-        </li>
-
-        <li>
-          <a href="">Noticias</a>
-        </li>
-
-        <li>
-          <a href="">Reclamaciones</a>
-        </li>
-
-        <li>
-          <a href="">Condiciones generales</a>
-        </li>
-
-        <li>
-          <a href="">Información de empresa</a>
-        </li>
-
-        <li>
-          <a href="">Politica de privacidad</a>
-        </li>
+        <li>Ofertas</li>
+        <li>Baloncesto</li>
+        <li>Balonmano</li>
+        <li>Béisbol</li>
+        <li>Boxeo/MMA</li>
+        <li>Carreras de caballos</li>
+        <li>Carreras de galgos</li>
+        <li>Ciclismo</li>
+        <li>Dardos</li>
+        <li>Deportes de invierno</li>
+        <li>Especiales</li>
+        <li>eSports</li>
+        <li>Fútbol</li>
+        <li>Fútbol americano</li>
+        <li>Futbol sala</li>
+        <li>Golf</li>
+        <li>Hockey sobre hielo</li>
+        <li>Motor</li>
+        <li>Política</li>
+        <li>Rugby League</li>
+        <li>Rugby Union</li>
+        <li>Snooker</li>
+        <li>Speedway</li>
+        <li>Tenis</li>
+        <li>Tenis de mesa</li>
+        <li>Trotones</li>
+        <li>Voleibol</li>
+        <li>Waterpolo</li>
+        <li>Más deportes</li>
+        <li>Cuotas menores de 2.00</li>
+        <li>Eventos populares</li>
       </ul>
-    </nav>
-  </footer>
-    
+    </aside>
+
+    <div class="container-articles-offers">
+      <h3>OFERTAS</h3>
+
+      <article>
+        <li>Mei eu mollis albucius, ex nisl contentiones</li>
+        <p>
+          Id sea utamur aperiam, te per choro accusamus consulatu. Brute munere
+        </p>
+        <li>Mei eu mollis albucius, ex nisl contentiones</li>
+        <p>
+          Id sea utamur aperiam, te per choro accusamus consulatu. Brute munere
+        </p>
+        <li>Mei eu mollis albucius, ex nisl contentiones</li>
+        <p>
+          Id sea utamur aperiam, te per choro accusamus consulatu. Brute munere
+        </p>
+      </article>
+    </div>
+
+    <div class="container-articles-functions">
+      <h3>FUNCIONES</h3>
+
+      <article>
+        <li>Mei eu mollis albucius, ex nisl contentiones</li>
+        <p>
+          Id sea utamur aperiam, te per choro accusamus consulatu. Brute munere
+        </p>
+        <li>Mei eu mollis albucius, ex nisl contentiones</li>
+        <p>
+          Id sea utamur aperiam, te per choro accusamus consulatu. Brute munere
+        </p>
+        <li>Mei eu mollis albucius, ex nisl contentiones</li>
+        <p>
+          Id sea utamur aperiam, te per choro accusamus consulatu. Brute munere
+        </p>
+      </article>
+    </div>
+
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a href="">Ayuda</a>
+          </li>
+
+          <li>
+            <a href="">Banca</a>
+          </li>
+
+          <li>
+            <a href="">Contáctenos</a>
+          </li>
+
+          <li>
+            <a href="">Juego responsable</a>
+          </li>
+
+          <li>
+            <a href="">Afiliados</a>
+          </li>
+
+          <li>
+            <a href="">Noticias</a>
+          </li>
+
+          <li>
+            <a href="">Reclamaciones</a>
+          </li>
+
+          <li>
+            <a href="">Condiciones generales</a>
+          </li>
+
+          <li>
+            <a href="">Información de empresa</a>
+          </li>
+
+          <li>
+            <a href="">Politica de privacidad</a>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-
 export default {
-  name: 'Home',
-  components: {
-    
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
 
 <style scoped>
-  header nav ul {
-    width: 100vw;
-    height: 10vh;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    background-color: var(--main-color);
-  
-  }
+header {
+  height: 10vh;
+  background-color: var(--main-color);
+}
 
-  header nav ul li:nth-child(2) {
-    color: white;
-    margin-right: 0;
-  }
+header nav {
+  height: inherit;
+}
 
-  header nav ul li:nth-child(3) {
-    border: 1px solid white;
-    padding: 1.5vh;
-    margin-right: 25vw;
-  }
+header nav ul {
+  height: inherit;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 
-  .img-logo {
-    width: 12vw;
-  }
+header nav ul li {
+  height: inherit;
+  padding: 1.7vh;
+}
 
-  main {
-    height: 200vh;
-  }
+header nav ul li:nth-child(2),
+header nav ul li:nth-child(3) {
+  height: inherit;
+  padding: 3.2vh;
+}
 
-  main section {
-    height: 110vh;
-    width: 100vw;
-    display: grid;
-    grid-template-areas:
-        "h h1"
-        "m m";
-    grid-template-columns: 25vw 75vw;
-    grid-template-rows: 100vh 25vh;
-  }
+header nav ul li:nth-child(2) a {
+  color: white;
+  padding: 2vw;
+}
 
-  main section article:first-child {
-    display: grid;
-    grid-area: "h";
-  }
+header nav ul li:nth-child(3) a {
+  padding: 1vh;
+  color: white;
+}
 
-  main section article:nth-child(2) {
-    display: grid;
-    grid-area: "h1";
-  }
+header nav ul li:nth-child(3) a:nth-child(2) {
+  border-radius: 5px;
+  background-color: var(--secondary-color);
+  color: #505050;
+}
 
-  main section article:nth-child(2) figure img {
-    height: 100vh;
-  }
+header nav ul li figure {
+  height: inherit;
+}
 
-  main section article:nth-child(3) {
-    display: grid;
-    grid-area: "m";
-  }
+header nav ul li figure img {
+  height: 6vh;
+}
 
-  footer {
-    height: 25vh;
-  }
+.grid-container {
+  width: 100vw;
+  display: grid;
+  grid-template-columns: 16vw 84vw;
+  grid-template-rows: 4fr;
+}
+
+.grid-container main {
+  grid-column-start: 2;
+  grid-column-end: 3;
+  grid-row-start: 1;
+  grid-row-end: 4;
+}
+
+.grid-container .container-articles-offers {
+  grid-column-start: 2;
+  grid-column-end: 3;
+}
+
+.grid-container .container-articles-functions {
+  grid-column-start: 2;
+  grid-column-end: 3;
+}
+
+.grid-container aside {
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-row-start: 1;
+  grid-row-end: 7;
+  background-color: #383838;
+}
+
+.grid-container aside ul {
+  padding: 3vh;
+}
+
+.grid-container aside ul li {
+  padding: 2vh;
+  font-size: 0.8rem;
+  color: #CECECE;
+}
+
+.grid-container footer {
+  grid-column-start: 2;
+  grid-column-end: 3;
+}
+
+main {
+  display: grid;
+  grid-template-columns: 50vw 34vw;
+  grid-template-rows: 20vh 1fr;
+}
+
+main section:nth-child(1) {
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-row-start: 1;
+  grid-row-end: 2;
+  background-color: #282828;
+}
+
+main section:nth-child(1) article {
+  font-size: 4px;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  padding: 2vh;
+}
+
+main section:nth-child(1) article h4 {
+  color: var(--main-color);
+  font-size: 0.8rem;
+  width: 20vw;
+}
+
+main section:nth-child(1) article .atom_p_firstChild_bet {
+  color: #ffff;
+  font-size: 1rem;
+  width: 20vw;
+  padding: 0.5vh 0;
+}
+
+main section:nth-child(1) article a {
+  border-radius: 5px;
+  background-color: var(--secondary-color);
+  color: #505050;
+  width: 5vw;
+  font-size: 0.8rem;
+  text-align: center;
+  padding: 0.8vh 0;
+}
+
+main section:nth-child(1) article .atom_p_secondChild_bet {
+  font-size: 0.6rem;
+  width: 40vw;
+  padding: 1.2vh 0;
+  color: #B5B5B5;
+}
+
+main section:nth-child(1) article figure {
+  margin: 0 0 0 43vw;
+  margin-top: 0;
+}
+
+main section:nth-child(1) article figure img {
+  margin-right: 0;
+  width: 100px;
+  height: 70px;
+  display: inline-block;
+}
+
+main section:nth-child(2) {
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-row-start: 1;
+  grid-row-end: 3;
+  background-color: #434343;
+}
+
+main section:nth-child(2) h4 {
+  color: #9effe0;
+  padding: 2vh;
+}
+
+main section:nth-child(2) article:nth-child(2) {
+  color: #DDDDDD;
+  padding: 2vh;
+}
+
+main section:nth-child(2) article:nth-child(3) {
+  color: #DDDDDD;
+  padding: 2vh;
+}
+
+main section:nth-child(2) article:nth-child(4) {
+  color: #DDDDDD;
+  padding: 2vh;
+}
+
+main section:nth-child(2) article:nth-child(2) div,
+main section:nth-child(2) article:nth-child(3) div,
+main section:nth-child(2) article:nth-child(3) div,
+main section:nth-child(2) article:nth-child(4) div {
+  padding: 3vh;
+}
+
+main section:nth-child(3) {
+  grid-column-start: 2;
+  grid-column-end: 3;
+  grid-row-start: 1;
+  grid-row-end: 3;
+  background-color: #858585;
+}
+
+hr {
+  color: white;
+}
 
 </style>
